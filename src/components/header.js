dc1bd9b79fa9c7ad1227f223793adcd490eaabe4
snopeks <creativesnopeks@gmail.com>
2020-03-09 20:34:47 -0700

    begin website project

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => (
+    <div id="header">
+        <h1>Tiny Bites</h1>
+        <ul>
+            <li><a href="#">Baby Nutrition</a></li>
+            <li><a href="#">Recipes</a></li>
+            <li><a href="#">Resources</a></li>
+        </ul>
+    </div>
+)
+
+export default Header
---
+++ src/pages/Index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Home = () => (
+    <div>
+        <h2>Feeding your baby just got easier!</h2>
+        <p>We know you want to feed your baby well and ensure they grow up healthy and strong.</p>
+        <p>This site is for parents who want a quick and easy resource to remember key nutrients, easy ingredients, and recommended resources for your baby's meals</p>
+        <div className="container">
+            <div>
+                <h3>Nutrients!</h3>
+            </div>
+            <div>
+                <h3>Ingredients!</h3>
+            </div>
+            <div>
+                <h3>Resources!</h3>
+            </div>
+        </div>
+    </div>
+
+)
+
+export default Home;
