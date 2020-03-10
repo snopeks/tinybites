293232d8ecc375798b116f5a3ab58edc272cd4d7
snopeks <creativesnopeks@gmail.com>
2020-03-09 22:21:45 -0700

    add image for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import stephbaby from '../stephbaby.jpg';
 
 
 const About = () => (
     <div> 
         <h2>About Tiny Bites</h2>
-        <p>Tiny Bites is a research project by a mom who got tired of constantly second-guessing herself about what to feed her baby.</p>
+        <img src={stephbaby}></img>
+        <p>I made Tiny Bites because I got tired of constantly second-guessing myself about what to feed my baby.</p>
+        <p>With all the people offering conflicting advice, and the myriad of random baby food recipes on the internet, I decided to investigate all the details myself. And I thought, there are probably others who could benefit from a concise research project that covers the nutrient needs of babies from 6-12 months.</p>
+        <p><i>Please note</i>, I am not a nutritionist, a doctor, or a dietician! I have simply researched this topic and pulled together all the information in one place.</p>
+
+
     </div>
 )
 
