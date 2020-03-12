0a48588b766577e231f922b66a42c8b636d378ae
snopeks <creativesnopeks@gmail.com>
2020-03-11 21:21:25 -0700

    adding content to easymeals page

--- src/pages/EasyMeals.js
+++ src/pages/EasyMeals.js
@@ -3,42 +3,68 @@ import React from 'react';
 const EasyMeals = () => (
     <div className="content">
         <h2>Meal Ideas</h2>
-        <p>These meals are based on a philosophy called baby led feeding. Essentially it means we feed babies whole foods (aka not pureeing them).</p>
+        <p>These meals are based on a philosophy called <i>baby led feeding</i>. Essentially it means we feed babies whole foods (aka not pureeing them).</p>
         <p>As long as the foods are prepared to an age appropriate texture and size, it is no more likely to be a choking hazard than purees!</p>
-        <h3>Meal basics</h3>
-        <p>Iron-fortified baby cereal</p>
-        <p>Pasta</p>
-        <p>-Legume-based for iron</p>
-        <p>-Regular for calories</p>
-        <p>Butter or oil-sauteed vegetables</p>
+        <p>An important part of baby led feeding is allowing your child to choose how much to eat. Every baby has their own appetite level, and in most cases we can trust them to decide when they are full.</p>
+        <p><i>If you have any concerns about your baby's growth or nutrition, please discuss with your health care provider!</i></p>
+        <div>
+            <h3>Meal basics</h3>
+            <h4>Simple go-to options for busy parents</h4>
+            <p>Iron-fortified baby cereal &amp; banana with nut butter</p>
+            <p>Pasta with Peas &amp; Yogurt sauce (legume-based pasta for added iron)</p>
+            <p>Butter or oil-sauteed vegetables &amp; nut butter toast</p>
+        </div>
+        <h3>Nutrition-boosting Toppings</h3>
+        <div className="container">
+            <p>Nutritional Yeast (b vitamins)</p>
+            <p>Hemp hearts (omega 3s, protein, iron)</p>
+            <p>Chia seeds (omega 3s, protein, fiber, calcium)</p>
+            <p>Flax meal (omega 3s, fiber)</p>
+        </div>
+        <h3>Healthy Sauces</h3>
         <div className="container">
             <div>
-                <p>Baby Pancakes</p>
+                <h4>Nut butter sauce</h4>    
+                <p>Combine 1-2 tsp of nut butter with 1-2 tsp of oil to thin, then drizzle on whatever foods you are serving! </p>
             </div>
             <div>
-                <p>Simple Stirfry</p>
+                <h4>Yogurt sauce</h4>
+                <p>Combine 1-2 TBSP of 4%+ M.F. yogurt with 1 TBSP soft tofu, mix till mostly smooth</p>
             </div>
-            <div>
-                <p>Superfood Combo</p>
+        </div>
+        <div>
+            <h3>Quick and Easy Recipes</h3>
+            <h4>Healthy meals in ~30min</h4>
+            <div className="container">
+                <div>
+                    <p>Baby Pancakes</p>
+                </div>
+                <div>
+                    <p>Simple Stirfry</p>
+                </div>
+                <div>
+                    <p>Superfood Combo</p>
+                </div>
+                <div>
+                    <p>Fruity &amp; Delicious</p>
+                </div>
+                <div>
+                    <p>Tasty Lentils</p>
+                </div>
+                <div>
+                    <p>Veggie Muffins</p>
+                </div>
+                <div>
+                    <p>Eggs'n'Things</p>
+                </div>
+                <div>
+                    <p>Mashed Potatoes</p>
+                </div>
+                <div>
+                    <p>Roasty Veg</p>
+                </div>
             </div>
-            <div>
-                <p>Fruity &amp; Delicious</p>
-            </div>
-            <div>
-                <p>Tasty Lentils</p>
-            </div>
-            <div>
-                <p>Veggie Muffins</p>
-            </div>
-            <div>
-                <p>Eggs'n'Things</p>
-            </div>
-            <div>
-                <p>Mashed Potatoes</p>
-            </div>
-            <div>
-                <p>Roasty Veg</p>
-            </div>
+
         </div>
     </div>
 )
