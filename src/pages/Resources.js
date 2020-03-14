950cc14f3da628d525d061f80c664d8416f51b54
snopeks <creativesnopeks@gmail.com>
2020-03-13 21:47:17 -0700

    add stars

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -8,10 +8,11 @@ const Resources = () => (
             <p>Don't just take my word for it, read up on this subject yourself, that way you'll feel confident from the inside out!</p>
         </div>
         <div>
-            <div>Websites</div>
-            <div>Books</div>
-            <div>Instagram</div>
-            <div>Other</div>
+            <h3><a href="https://plantbasedjuniors.com/">Plant-Based Juniors</a></h3>
+            <p><a href="https://www.instagram.com/plantbasedjuniors/">@plantbasedjuniors</a></p>
+            <h3><a href="https://newwaysnutrition.com/">New Ways Nutrition</a></h3>
+            <p><a href="https://www.instagram.com/newwaysnutrition/">@newwaysnutrition</a></p>
+            <h3><a href="https://www.healthlinkbc.ca/healthy-eating/vegetarian-baby-6-12-month">HealthLink BC</a></h3>
         </div>
     </div>
 )
