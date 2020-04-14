d5ce95e7639e032df68a5ad658e6410872431350
snopeks <creativesnopeks@gmail.com>
2020-04-13 22:16:44 -0700

    update header img and resource page design

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -4,19 +4,41 @@ const Resources = () => (
     <div className="content">
         <div className="text">
             <h2>Resources</h2>
+            <p>Don't just take my word for it, read up on this subject yourself, that way you'll feel confident from the inside out.</p>
             <p>Here are some of my favorite baby feeding resources.</p>
-            <p>Don't just take my word for it, read up on this subject yourself, that way you'll feel confident from the inside out!</p>
         </div>
-        <div id="resourceLinks" className="infoLeft">
-            <p><a href="https://plantbasedjuniors.com/">Plant-Based Juniors</a></p>
-            <p><a href="https://www.instagram.com/plantbasedjuniors/">@plantbasedjuniors</a></p>
-            <p><a href="https://newwaysnutrition.com/">New Ways Nutrition</a></p>
-            <p><a href="https://www.instagram.com/newwaysnutrition/">@newwaysnutrition</a></p>
-            <p><a href="https://www.babyledweaningideas.com/carrot-kale-cheese-muffins/">Baby Led Weaning Ideas</a></p>
-            <p><a href="https://mylittleeater.com/my-top-6-starter-finger-foods-for-baby-led-weaning/">My Little Eater</a></p>
-            <p><a href="https://www.healthlinkbc.ca/healthy-eating/vegetarian-baby-6-12-month">HealthLink BC</a></p>
-            <p><a href="https://www.healthyfamiliesbc.ca/home/blog/introducing-solids-your-baby-look-baby-led-weaning">Healthy Families BC</a></p>
-            <p><a href="https://www.webmd.com/parenting/baby/introducing-new-foods#1">WebMD Baby Allergies</a></p>
+        <div id="resourceContainer" className="container">
+            <div>
+                <h3>Plant Based Juniors</h3>
+                <p><a href="https://plantbasedjuniors.com/">Plant-Based Juniors</a></p>
+                <p><a href="https://www.instagram.com/plantbasedjuniors/">@plantbasedjuniors</a></p>
+
+            </div>
+            <div>
+                <h3>New Ways Nutrition</h3>
+                <p><a href="https://newwaysnutrition.com/">New Ways Nutrition</a></p>
+                <p><a href="https://www.instagram.com/newwaysnutrition/">@newwaysnutrition</a></p>
+            </div>
+            <div>
+                <h3>Baby Led Weaning Ideas</h3>
+                <p><a href="https://www.babyledweaningideas.com/carrot-kale-cheese-muffins/">Baby Led Weaning Ideas</a></p>
+            </div>
+            <div>
+                <h3>My Little Eater</h3>
+                <p><a href="https://mylittleeater.com/my-top-6-starter-finger-foods-for-baby-led-weaning/">My Little Eater</a></p>
+            </div>
+            <div>
+                <h3>HealthLink BC</h3>
+                <p><a href="https://www.healthlinkbc.ca/healthy-eating/vegetarian-baby-6-12-month">HealthLink BC</a></p>
+            </div>
+            <div>
+                <h3>Healthy Families BC</h3>
+                <p><a href="https://www.healthyfamiliesbc.ca/home/blog/introducing-solids-your-baby-look-baby-led-weaning">Healthy Families BC</a></p>
+            </div>
+            <div>
+                <h3>WebMD - baby allergies</h3>
+                <p><a href="https://www.webmd.com/parenting/baby/introducing-new-foods#1">WebMD Baby Allergies</a></p>
+            </div>
         </div>
     </div>
 )
